2639ff3404f6fc2b55db6ade81920d1dc15b8941
BroascaGeorge <georgebroasca98@gmail.com>
2023-05-27 22:08:40 +0300

    cart

--- src/App.js
+++ src/App.js
@@ -3,13 +3,28 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Amazon from "./components/Amazon";
 import Cart from "./components/Cart";
+import "./styles/amazon.css";
 
 function App() {
   const [show, setShow] = useState(true);
   const [cart, setCart] = useState([]);
+  const [warning, setWarning] = useState(false);
 
   const handleClick = (item) => {
-    console.log(item);
+    let isPresent = false;
+    cart.forEach((product) => {
+      if (item.id === product.id) {
+        isPresent = true;
+      }
+    });
+    if (isPresent) {
+      setWarning(true);
+      setTimeout(() => {
+        setWarning(false);
+      }, 2000);
+      return;
+    }
+    setCart([...cart, item]);
   };
 
   return (
@@ -17,6 +32,9 @@ function App() {
       <Navbar size={cart.length} />
       <Amazon handleClick={handleClick} />
       <Cart />
+      {warning && (
+        <div className="warning">Item is already added to your cart.</div>
+      )}
     </>
   );
 }
